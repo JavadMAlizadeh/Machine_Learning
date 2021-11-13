353735815c65340731994feb7b37608fc8b2014d
Javad Mohammad Alizadeh <javad.mohammadalizadeh@gmail.com>
2021-11-13 14:37:06 +0330

    Create normalEqn.m

---
+++ normalEqn.m
@@ -0,0 +1,21 @@
+function [theta] = normalEqn(X, y)
+%NORMALEQN Computes the closed-form solution to linear regression 
+%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%   regression using the normal equations.
+
+X = [1; 2; 3];
+y = [5; 11; 19];
+
+theta = zeros(size(X, 2), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the code to compute the closed form solution
+%               to linear regression and put the result in theta.
+%
+
+
+theta = pinv(X'*X)*(X'*y)
+
+% ============================================================
+
+end
